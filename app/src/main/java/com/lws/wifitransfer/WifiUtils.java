a5d2fa339442e6c18efe63739ea7fbe1570250a9
luwenshan <lws1076694749@126.com>
2018-03-22 19:16:55 +0800

    Initial commit

---
+++ app/src/main/java/com/lws/wifitransfer/WifiUtils.java
@@ -0,0 +1,47 @@
+package com.lws.wifitransfer;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+
+/**
+ * Created by masel on 2016/10/10.
+ */
+
+public class WifiUtils {
+
+    public static String getWifiIp(Context context) {
+        context = context.getApplicationContext();
+        WifiManager wifimanager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInfo = wifimanager.getConnectionInfo();
+        if (wifiInfo != null) {
+            return intToIp(wifiInfo.getIpAddress());
+        }
+        return null;
+    }
+
+    private static String intToIp(int i) {
+        return (i & 0xFF) + "." + ((i >> 8) & 0xFF) + "." + ((i >> 16) & 0xFF) + "." + ((i >> 24) & 0xFF);
+    }
+
+    public static int getWifiState(Context context) {
+        context = context.getApplicationContext();
+        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        if (wifiManager != null) {
+            return wifiManager.getWifiState();
+        }
+        return WifiManager.WIFI_STATE_DISABLED;
+    }
+
+    public static NetworkInfo.State getWifiConnectState(Context context) {
+        context = context.getApplicationContext();
+        ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mWiFiNetworkInfo = manager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (mWiFiNetworkInfo != null) {
+            return mWiFiNetworkInfo.getState();
+        }
+        return NetworkInfo.State.DISCONNECTED;
+    }
+}
